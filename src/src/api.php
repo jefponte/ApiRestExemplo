476ea5a73b8876b3012652a6366583c9700282b9
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2020-03-10 14:12:43 -0300

    Envio usando GET>

---
+++ src/src/api.php
@@ -0,0 +1,32 @@
+<?php 
+
+function __autoload($classe) {
+    
+    if (file_exists ( 'classes/dao/' . $classe . '.php' )){
+        include_once 'classes/dao/' . $classe . '.php';
+    }
+    else if (file_exists ( 'classes/model/' . $classe . '.php' )){
+        include_once 'classes/model/' . $classe . '.php';
+    }
+    else if (file_exists ( 'classes/controller/' . $classe . '.php' )){
+        include_once 'classes/controller/' . $classe . '.php';
+    }
+    else if (file_exists ( 'classes/util/' . $classe . '.php' )){
+        include_once 'classes/util/' . $classe . '.php';
+    }
+    else if (file_exists ( 'classes/view/' . $classe . '.php' )){
+        include_once 'classes/view/' . $classe . '.php';
+    }
+}
+
+
+$controller = new InfoController();
+
+if(isset($_GET['enviar_info'])){
+    $controller->cadastrarApiGET();
+    
+}else{
+    $controller->listarJSON();
+}
+
+?>
